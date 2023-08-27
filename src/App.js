7683ecb3f5d389ced9f70d0a2a92081d8e247c35
Polina Radahuz <polina.radahuz@gmail.com>
2023-08-27 23:45:18 +0200

    added source link

--- src/App.js
+++ src/App.js
@@ -4,8 +4,18 @@ import DisplayWeather from "./DisplayWeather";
 
 export default function App() {
   return (
-    <div className="App container">
-      <DisplayWeather city="Hannover" />
+    <div className="App">
+      <div className="container">
+        <div className="weather-app">
+          <DisplayWeather city="Hannover" />
+        </div>
+        <div className="source-link">
+          <a href="https://github.com/pollyshoot/weather-app-react">
+            Open-source code
+          </a>
+          , by Polina Radahuz
+        </div>
+      </div>
     </div>
   );
 }
